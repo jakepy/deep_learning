de916f8bd5a365a856309b9b23aa6fedc24d3983
JakePy <101231593+jakepy@users.noreply.github.com>
2022-06-29 20:57:34 -0700

    Create faux_houses.py

---
+++ faux_houses.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+
+def house_model():
+    ### START CODE HERE
+    
+    # Define input and output tensors with the values for houses with 1 up to 6 bedrooms
+    # Hint: Remember to explictly set the dtype as float
+    xs = [0, 1, 2, 3, 4, 5]
+    ys = [.5, 1.0, 1.5, 2.0, 2.5, 3.0]
+    
+    # Define your model (should be a model with 1 dense layer and 1 unit)
+    model = keras.Sequential([
+        keras.layers.Dense(
+            units=1,
+            input_shape=[1]
+        )
+    ])
+    
+    # Compile your model
+    # Set the optimizer to Stochastic Gradient Descent
+    # and use Mean Squared Error as the loss function
+    model.compile(optimizer='sgd', loss='mean_squared_error')
+    
+    # Train your model for 1000 epochs by feeding the i/o tensors
+    model.fit(xs, ys, epochs=1000)
+    
+    ### END CODE HERE
+    return model
+    
+# Get your trained model
+model = house_model()
+
+new_y = 7.0
+prediction = model.predict([new_y])[0]
+print(prediction)
+
+## EXTRA If everything went as expected you should see a prediction value very close to 4. If not, try adjusting your code before submitting the assignment. Notice that you can play around with the value of new_y to get different predictions. In general you should see that the network was able to learn the linear relationship between x and y, so if you use a value of 8.0 you should get a prediction close to 4.5 and so on.
